c463e7559aef51b7509192813e38f4390659482f
lmwsky <lmwtclmwtc@163.com>
2020-10-21 10:45:32 +0800

    [update] update cli basic info

--- funcverbnet/cli.py
+++ funcverbnet/cli.py
@@ -6,9 +6,12 @@ import click
 @click.command()
 def main(args=None):
     """Console script for funcverbnet."""
-    click.echo("Replace this message by putting your code into "
-               "funcverbnet.cli.main")
-    click.echo("See click documentation at https://click.palletsprojects.com/")
+    click.echo(
+        "FuncVerbNet, Provides a knowledge system constructed from functionality categories, verbs, and "
+        "phrase patterns, as well as functionality for fine-grained analysis of functionality descriptions "
+        "based on this knowledge system."
+        )
+    # click.echo("See click documentation at https://click.palletsprojects.com/")
     return 0
 
 
