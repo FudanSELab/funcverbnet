df6e89b51a7108df7348cd23305802ee5fb6059a
lmwsky <lmwtclmwtc@163.com>
2020-10-29 10:18:33 +0800

    [add] add basic structure

--- funcverbnet/funcverbnet.py
+++ funcverbnet/funcverbnet.py
@@ -1,1 +1,11 @@
 """Main module."""
+
+
+class FuncVerbNet:
+    def __init__(self):
+        ## todo: load all the data from json.
+        self.roles=[]
+        self.cate=[]
+        self.verb=[]
+        pass
+
---
+++ funcverbnet/model.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+-----------------------------------------
+@Author: isky
+@Email: 19110240019@fudan.edu.cn
+@Created: 2020/10/21
+------------------------------------------
+@Modify: 2020/10/21
+------------------------------------------
+@Description: This file define the basic model class for FuncVerbNet.
+"""
+
+
+# TODO: define all the property of all those FuncCategory based on the OWL we define.
+
+class FuncCategory:
+    def __init__(self,id,name):
+        self.id=id
+        self.name=name
+        pass
+
+
+class FuncVerb:
+    def __init__(self):
+        pass
+
+
+class PhasePattern:
+    def __init__(self):
+        pass
+
+
+class Role:
+    def __init__(self):
+        pass
