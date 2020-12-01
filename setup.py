ec8c6df766bf0676b1d60eeb3421e8714c63dc36
XHJ-Keai <xuhuanjun1@163.com>
2020-12-01 15:46:51 +0800

    [update] update import

--- setup.py
+++ setup.py
@@ -10,7 +10,8 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', ]
+requirements = ['Click>=7.0',
+                'pandas', ]
 
 setup_requirements = ['pytest-runner', ]
 
