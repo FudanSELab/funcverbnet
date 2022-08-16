821391431d63e24838a4c34bf49f9969f9866336
yiduyton <yiduyton@gmail.com>
2022-08-16 22:54:34 +0800

    Bump version: 0.2.17 → 0.2.18

--- setup.py
+++ setup.py
@@ -58,6 +58,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/FudanSELab/funcverbnet',
-    version='0.2.17',
+    version='0.2.18',
     zip_safe=False,
 )
